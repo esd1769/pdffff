595365c8015a86a7ca8f2398b3a6db306c8ab2a5
esd1769 <126pubgwarrior@gmail.com>
2025-11-05 12:42:55 -0800

    Create h4.java

---
+++ Download/src/h4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class h4 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Please enter the length of array 1: ");
+        int n1=sc.nextInt();
+        int[] arr1 =new int[n1];
+
+        System.out.println("Please enter the elements of the arr1:");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Please enter the length of array 2: ");
+        int n2=sc.nextInt();
+        int[] arr2 = new int[n2];
+
+        System.out.println("Please enter the elements of the arr2:");
+        for (int i=0;i <n2;i++){
+            arr2[i] =sc.nextInt();
+        }
+
+        boolean isSubset =true;
+
+        for (int i =0; i< n2;i++) {
+            boolean fou =false;
+            for (int j =0; j< n1;j++) {
+                if (arr2[i] ==arr1[j]) {
+                    fou = true;
+                    break;
+                }//subset-unique,have no sequence..2,5,7..or 5,7,2 same
+            }
+            if (!fou){
+                isSubset= false;
+                break;
+            }
+        }
+
+        if (isSubset){
+            System.out.println("Array 2 is a subset of Array 1.");
+        } else{
+            System.out.println("Array 2 is not a subset of Array 1.");
+        }
+    }
+}
